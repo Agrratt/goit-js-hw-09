d1d7bb87e4c88b47b92451de2331bb1d5c8355b8
Kateryna Tolstykh <agrratt@gmail.com>
2022-01-03 17:21:12 +0200

    add 1-exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+    btnStart: document.querySelector("button[data-start]"),
+  btnStop: document.querySelector("button[data-stop]"),
+    body: document.querySelector('body'),
+};
+
+refs.btnStart.addEventListener('click', startRandomColor);
+refs.btnStop.addEventListener('click', stopRandomColor);
+
+refs.btnStop.disabled = true;
+let timerId = 0;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeColorBody() { 
+  refs.body.style.background = getRandomHexColor()
+};
+
+function startRandomColor() {
+  timerId = setInterval(changeColorBody, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+};
+
+function stopRandomColor() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+  refs.btnStop.disabled = true;
+};
 
